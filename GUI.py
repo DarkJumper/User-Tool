17a20a5535b3c37e9650af900550c46d3f692b23
DarkJumper <p.schwarz1994@outlook.de>
2019-09-05 15:49:32 +0200

    Trend Auswertungs Frame angepasst aufruf der Funktion fehlt noch

--- GUI.py
+++ GUI.py
@@ -2,12 +2,13 @@ import tkinter as tk
 from tkinter import filedialog
 import FGR_Rename
 
-#Klasse definieren
+
+# Klasse definieren
 class App:
     # root/master initialisieren
     def __init__(self):
         self.master = tk.Tk()
-        self.master.protocol("WM_DELETE_WINDOW", self.quit_app) #Abspeichern und Beenden des Frames
+        self.master.protocol("WM_DELETE_WINDOW", self.quit_app)  # Abspeichern und Beenden des Frames
         self.master.title("Freelance Tool - copyright Peter Schwarz Aug 2019")
         self.set_main_frame()
 
@@ -17,18 +18,18 @@ class App:
         self.main_frame = tk.Frame(self.master)
         self.main_frame.pack()
         # Widgets initialisieren
-        self.quit_button = tk.Button(self.main_frame, text="Beenden", height= 1, width= 20, activebackground='#FF0000',
-                                    command= self.quit_app)
-        self.SFP_button = tk.Button(self.main_frame, text="SFP Auswerten",height= 1, width= 20, background='#58ACFA',
-                                    command= self.SFP_Auswerten)
-        self.rename_button = tk.Button(self.main_frame, text="Rename Grafik",height= 1, width= 20, background='#2EFEC8',
-                                    command=self.Rename_Grafik_Frame)
-        self.trend_button = tk.Button(self.main_frame, text="Trend Auswerten",height= 1, width= 20, background='#BFFF00',
-                                    command= self.Trend_Auswerten)
-        self.PRT_button = tk.Button(self.main_frame, text="PRT Erstellen",height= 1, width= 20, background='#FAAC58',
-                                    command = self.Prt_Erstellen)
-        self.Info_button = tk.Button(self.main_frame, text="INFO!",height= 1, width= 20, background='#F7FE2E',
-                                    command = self.INFO)
+        self.quit_button = tk.Button(self.main_frame, text="Beenden", height=1, width=20, activebackground='#FF0000',
+                                     command=self.quit_app)
+        self.SFP_button = tk.Button(self.main_frame, text="SFP Auswerten", height=1, width=20, background='#58ACFA',
+                                    command=self.SFP_Auswerten)
+        self.rename_button = tk.Button(self.main_frame, text="Rename Grafik", height=1, width=20, background='#2EFEC8',
+                                       command=self.Rename_Grafik_Frame)
+        self.trend_button = tk.Button(self.main_frame, text="Trend Auswerten", height=1, width=20, background='#BFFF00',
+                                      command=self.Trend_Auswerten)
+        self.PRT_button = tk.Button(self.main_frame, text="PRT Erstellen", height=1, width=20, background='#FAAC58',
+                                    command=self.Prt_Erstellen)
+        self.Info_button = tk.Button(self.main_frame, text="INFO!", height=1, width=20, background='#F7FE2E',
+                                     command=self.INFO)
         self.copyright = tk.Label(self.main_frame, text='Copyright: Peter Schwarz', font=('Arial', 8))
         # Widgets positionieren
         self.SFP_button.grid(row=0, column=0)
@@ -50,23 +51,27 @@ class App:
         self.rename_frame.pack()
         # Widgets initialisieren
         self.head_label_Prt = tk.Label(self.rename_frame, text='Grafikbilder Umbennen')
-        self.Durchsuchen_button = tk.Button(self.rename_frame, text="Durchsuchen",height= 1, width= 20,
+        self.Durchsuchen_button = tk.Button(self.rename_frame, text="Durchsuchen", height=1, width=20,
                                             command=self.file_dialog)
-        self.Rename_button = tk.Button(self.rename_frame, text="Auführen!", height=1, width=20, activebackground='#40FF00',
-                                            command=self.Rename_Ausführen)
-        self.setting_entry = tk.Entry(self.rename_frame,width="50")
-        self.safe_settings_button = tk.Button(self.rename_frame, text="Zurück!", height= 1, width= 20, background='#FE2E2E',
-                                            command=self.safe_settings_RENAME)
+        self.Rename_button = tk.Button(self.rename_frame, text="Auführen!", height=1, width=20,
+                                       activebackground='#40FF00',
+                                       command=self.Rename_Ausführen)
+        self.setting_entry = tk.Entry(self.rename_frame, width="50")
+        self.safe_settings_button = tk.Button(self.rename_frame, text="Zurück!", height=1, width=20,
+                                              background='#FE2E2E',
+                                              command=self.safe_settings_RENAME)
         self.var = tk.StringVar()
         self.var1 = tk.StringVar()
         self.var2 = tk.StringVar()
-        self.Checkbox_PNG = tk.Checkbutton(self.rename_frame, text='PNG',offvalue="", onvalue="PNG", variable=self.var)
-        self.Checkbox_JPG = tk.Checkbutton(self.rename_frame, text='JPG',offvalue="", onvalue="JPG", variable=self.var1)
-        self.Checkbox_GIF = tk.Checkbutton(self.rename_frame, text='GIF',offvalue="", onvalue="GIF", variable=self.var2)
+        self.Checkbox_PNG = tk.Checkbutton(self.rename_frame, text='PNG', offvalue="", onvalue="PNG", variable=self.var)
+        self.Checkbox_JPG = tk.Checkbutton(self.rename_frame, text='JPG', offvalue="", onvalue="JPG",
+                                           variable=self.var1)
+        self.Checkbox_GIF = tk.Checkbutton(self.rename_frame, text='GIF', offvalue="", onvalue="GIF",
+                                           variable=self.var2)
         # Widgets positionieren
         self.head_label_Prt.grid(row=0, column=0, columnspan=2)
         self.Durchsuchen_button.grid(row=1, column=0)
-        self.setting_entry.grid(row=1, column=1,padx=5,pady=10,ipady=3)
+        self.setting_entry.grid(row=1, column=1, padx=5, pady=10, ipady=3)
         self.Checkbox_PNG.grid(row=2, column=0)
         self.Checkbox_JPG.grid(row=3, column=0)
         self.Checkbox_GIF.grid(row=4, column=0)
@@ -83,7 +88,7 @@ class App:
         self.SFP_frame.pack()
         self.head_label_SFP = tk.Label(self.SFP_frame, text='SFP Auswerten ist noch in Arbeit!')
         self.safe_settings_button_SFP = tk.Button(self.SFP_frame, text="Zurück!", height=1, width=20,
-                                                command=self.safe_settings_SFP)
+                                                  command=self.safe_settings_SFP)
         self.head_label_SFP.grid(row=0, column=0, columnspan=2)
         self.safe_settings_button_SFP.grid(row=3, column=1)
         self.master.mainloop()
@@ -94,39 +99,39 @@ class App:
         # Hauptframe schließen
         self.main_frame.destroy()
         self.Trend_frame.pack()
-        self.head_label_trend = tk.Label(self.Trend_frame, text='Trends Auswerten!',font=('Arial', 10))
-        self.label_date = tk.Label(self.Trend_frame,text="Datum",font=('Arial', 8))
-        self.label_time = tk.Label(self.Trend_frame, text="Zeit",font=('Arial', 8))
-        self.label_begin = tk.Label(self.Trend_frame,text="Von",font=('Arial', 8))
-        self.label_end = tk.Label(self.Trend_frame, text="bis",font=('Arial', 8))
-        self.safe_settings_button_Trend = tk.Button(self.Trend_frame, text="Zurück!", height=1, width=10,background='#FE2E2E',
-                                            command=self.safe_settings_Trend)
+        self.head_label_trend = tk.Label(self.Trend_frame, text='Trends Auswerten!', font=('Arial', 10))
+        self.label_date = tk.Label(self.Trend_frame, text="Datum", font=('Arial', 8))
+        self.label_time = tk.Label(self.Trend_frame, text="Zeit", font=('Arial', 8))
+        self.label_begin = tk.Label(self.Trend_frame, text="Von", font=('Arial', 8))
+        self.label_end = tk.Label(self.Trend_frame, text="bis", font=('Arial', 8))
+        self.label_MSR = tk.Label(self.Trend_frame, text="MSR Stellen", font=('Arial', 8))
+        self.safe_settings_button_Trend = tk.Button(self.Trend_frame, text="Zurück!", height=1, width=10,
+                                                    background='#FE2E2E',
+                                                    command=self.safe_settings_Trend)
         self.button_Trend_Auswerten = tk.Button(self.Trend_frame, text="Auswerten!", height=1, width=10)
-        self.setting_entry_Date_begin = tk.Entry(self.Trend_frame, width="9")
-        self.setting_entry_Date_end = tk.Entry(self.Trend_frame, width="9")
-        self.setting_entry_time_begin = tk.Entry(self.Trend_frame, width="9")
-        self.setting_entry_time_end = tk.Entry(self.Trend_frame, width="9")
-        self.setting_entry_MSR_1 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_MSR_2 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_MSR_3 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_MSR_4 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_MSR_5 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_MSR_6 = tk.Entry(self.Trend_frame, width="12")
-        self.setting_entry_Date_begin.insert(8,'TT.MM.JJ')
+        self.setting_entry_Date_begin = tk.Entry(self.Trend_frame, width="10")
+        self.setting_entry_Date_end = tk.Entry(self.Trend_frame, width="10")
+        self.setting_entry_time_begin = tk.Entry(self.Trend_frame, width="10")
+        self.setting_entry_time_end = tk.Entry(self.Trend_frame, width="10")
+        self.MSR_entry = tk.Entry(self.Trend_frame, width="35")
+        self.setting_entry_Date_begin.insert(8, 'TT.MM.JJ')
         self.setting_entry_Date_end.insert(8, 'TT.MM.JJ')
-        self.setting_entry_time_begin.insert(8,'HH.MM.SS')
+        self.setting_entry_time_begin.insert(8, 'HH.MM.SS')
         self.setting_entry_time_end.insert(8, 'HH.MM.SS')
+        self.MSR_entry.insert(8, 'P1000,L1000,...')
         self.head_label_trend.grid(row=0, column=0, columnspan=5)
-        self.button_Trend_Auswerten.grid(row=2, column=4)
-        self.label_date.grid(row=1,column=2)
-        self.label_time.grid(row=1,column=3)
-        self.label_begin.grid(row=2,column=1)
-        self.label_end.grid(row=3,column=1)
-        self.setting_entry_Date_begin.grid(row=2, column=2, padx=5, pady=1, ipady=3)
-        self.setting_entry_Date_end.grid(row=3, column=2, padx=5, pady=1, ipady=3)
-        self.setting_entry_time_begin.grid(row=2, column=3, padx=5, pady=1, ipady=3)
-        self.setting_entry_time_end.grid(row=3, column=3, padx=5, pady=1, ipady=3)
-        self.safe_settings_button_Trend.grid(row=3, column=4)
+        self.button_Trend_Auswerten.grid(row=3, column=4)
+        self.label_date.grid(row=2, column=1)
+        self.label_time.grid(row=2, column=2)
+        self.label_begin.grid(row=3, column=0)
+        self.label_end.grid(row=4, column=0)
+        self.setting_entry_Date_begin.grid(row=3, column=1, padx=5, pady=1, ipady=3)
+        self.setting_entry_Date_end.grid(row=4, column=1, padx=5, pady=1, ipady=3)
+        self.setting_entry_time_begin.grid(row=3, column=2, padx=5, pady=1, ipady=3)
+        self.setting_entry_time_end.grid(row=4, column=2, padx=5, pady=1, ipady=3)
+        self.MSR_entry.grid(row=1, column=1, padx=1, pady=1, ipady=3, columnspan=5)
+        self.label_MSR.grid(row=1, column=0)
+        self.safe_settings_button_Trend.grid(row=4, column=4)
         self.master.mainloop()
 
     def Prt_Erstellen(self):
@@ -137,7 +142,7 @@ class App:
         self.Prt_frame.pack()
         self.head_label_Prt = tk.Label(self.Prt_frame, text='PRT Erstellen ist noch in Arbeit!')
         self.safe_settings_button_Prt = tk.Button(self.Prt_frame, text="Zurück!", height=1, width=20,
-                                            command=self.safe_settings_Prt)
+                                                  command=self.safe_settings_Prt)
         self.head_label_Prt.grid(row=0, column=0, columnspan=2)
         self.safe_settings_button_Prt.grid(row=3, column=1)
         self.master.mainloop()
@@ -150,7 +155,7 @@ class App:
         self.info_frame.pack()
         self.head_label_info = tk.Label(self.info_frame, text='NOCH PLATZHALTER')
         self.safe_settings_button_info = tk.Button(self.info_frame, text="Zurück!", height=1, width=20,
-                                            command=self.safe_settings_info)
+                                                   command=self.safe_settings_info)
         self.head_label_info.grid(row=0, column=0, columnspan=2)
         self.safe_settings_button_info.grid(row=3, column=1)
         self.master.mainloop()
@@ -164,7 +169,7 @@ class App:
     def file_dialog(self):
         filename = filedialog.askopenfilename(filetypes=(("Csv files", "*.csv;*.csv")
                                                          , ("All files", "*.*")))
-        self.setting_entry.insert(0,filename)
+        self.setting_entry.insert(0, filename)
 
     # Zum Hauptfenster zurückkehren
     def safe_settings_RENAME(self):
